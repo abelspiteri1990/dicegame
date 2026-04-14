79dff6458d7906c5341aa7663d783c892b6614d6
abelspiteri1990 <abel.spiteri@gmail.com>
2026-04-14 19:47:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//alert ("olej");
+
+var randomNumber1 = Math.floor(Math.random() * 6 ) + 1 ; //whole number between 1 to 6.
+var randomNumber2 = Math.floor(Math.random() * 6 ) + 1 ; //whole number between 1 to 6.
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png" ; //dice1.png-dice6.png
+var randomDiceImage2= "dice" + randomNumber2 + ".png" ; //dice1.png-dice6.png
+
+var randomImageSource1 = "images/" + randomDiceImage1; //images/dice1.png - images/dice6.png
+var randomImageSource2 = "images/" + randomDiceImage2; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img") [0] ;
+var image2 = document.querySelectorAll("img") [1] ;
+
+image1.setAttribute("src", randomImageSource1);
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2 ){
+    document.querySelector ("h1").innerHTML = "Player 1 Wins &#x261D !" ;
+    
+
+} else if (randomNumber1 < randomNumber2 ){
+    document.querySelector ("h1").innerHTML = "Player 2 Wins &#x270C !" ;
+
+} else {
+    document.querySelector ("h1").innerHTML = "DRAW &#x1F91D !" ;
+}
+
+let c = 5 ;
+
